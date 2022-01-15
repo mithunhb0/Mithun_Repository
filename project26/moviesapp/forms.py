7fc609bf84a5e155c8d14b28664759a809fad54d
Mithun H B <78333994+mfatal07@users.noreply.github.com>
2022-01-15 21:53:39 +0530

    Delete project26 directory

--- project26/moviesapp/forms.py
+++
@@ -1,9 +0,0 @@
-from moviesapp.models import movietable
-from django import forms
-
-class movietableForm(forms.ModelForm):
-
-    class Meta:
-
-        model = movietable
-        fields = ('id','moviename','hero','heroine')
